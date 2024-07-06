9655c859bc0cce1c4ce48c375f3d6ae6689505ae
Paguh Esatrio <97024224+PaguhEsatrio@users.noreply.github.com>
2024-07-06 21:38:50 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import Home from './src/screens/home'
+import AddNote from './src/screens/addNote'
+import EditNote from './src/screens/editNote'
+
+const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage, addNote, deleteNote, editNote, editedItem, setEditedItem }) => {
+  switch (currentPage) {
+    case 'home':
+      return (
+        <Home
+          noteList={noteList}
+          setCurrentPage={setCurrentPage}
+          deleteNote={deleteNote}
+          setEditedItem={setEditedItem}
+        />
+      )
+    case "add":
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />
+    case 'edit':
+      return <EditNote setCurrentPage={setCurrentPage} editedItem={editedItem} editNote={editNote} />
+    default:
+      return <Home />
+  }
+}
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('home')
+  const [editedItem, setEditedItem] = useState({})
+  const [noteList, setNoteList] = useState([
+    {
+      id: 1,
+      title: 'Note pertama',
+      desc:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+    },
+  ])
+
+  const addNote = (title, desc) => {
+    const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title: title,
+        desc: desc,
+      },
+    ]);
+  };
+
+  const deleteNote = (id) => {
+    const updateNoteList = noteList.filter((note) => {
+      return note.id != id;
+    })
+    setNoteList(updateNoteList)
+  }
+
+  function editNote(id, title, desc) {
+    const updatedNoteList = noteList.map((note) => {
+      if (note.id === id) {
+        note.title = title;
+        note.desc = desc;
+      }
+      return note;
+    });
+
+    setNoteList(updatedNoteList);
+  }
+
+  return (
+    <CurrentPageWidget
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+      noteList={noteList}
+      addNote={addNote}
+      deleteNote={deleteNote}
+      editNote={editNote}
+      editedItem={editedItem}
+      setEditedItem={setEditedItem}
+    />
+  )
+}
+
+export default App
